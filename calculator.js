12c0c25283f9aab4f5cebc5bd5892a2754013af5
TFrank <madmonsterHI@gmail.com>
2023-03-21 18:23:51 -0400

    calc now functions correctly

--- calculator.js
+++ calculator.js
@@ -1,75 +1,59 @@
-function add(a, b) {
-    return a + b;
-}
-function subtract(a, b) {
-    return a - b;
-}
-function multiply(a, b) {
-    return a * b;
-}
-function divide(a, b) {
-    return a / b;
-}
 function operate(operator, a, b) {
     if (operator === "add") {
-        return add(a, b);
+        return a + b;
     } else if (operator === "subtract") {
-        return subtract(a, b);
+        return a - b;
     } else if (operator === "multiply") {
-        return multiply(a, b);
+        return a * b;
     }else if (operator === "divide") {
-        return divide(a, b);
+        return a / b;
     }
 }
-
 let buttons = document.querySelectorAll('button');
 const screen = document.querySelector('#screen');
-let displayValue = [null, null];
+let a = null;
+let b = null;
+let c;
+const maxLength = 9;
 let operator;
-
 buttons.forEach(function (i) {
     i.addEventListener('click', function () {
-        const maxLength = 9;
-        if (this.className === "number"){
+        if (this.id === "AC") {
+            screen.textContent = ("");
+            a = null;
+            b = null;
+            c = null;
+        } else if (this.className === "number" && a === null){
             if (screen.textContent.length > maxLength){
                 this.textContent = this.textContent.substr(0, maxLength);
             } else {
                 screen.textContent += this.id.match(/\d+/);
             }
-        } else if (this.id === "AC") {
-            screen.textContent = ("");
-            displayValue = [null, null];
-        } else if (this.id === "add") {
-            operator = "add";
-            if (displayValue[0] === null) {
-                displayValue[0] = screen.textContent * 1;
-                screen.textContent = "";
+        } else if (this.className === "number" && !(a === null)){
+            screen.textContent = "";
+            c = null;
+            b = a;
+            a = null;
+            if (screen.textContent.length > maxLength){
+                this.textContent = this.textContent.substr(0, maxLength);
+            } else {
+                screen.textContent += this.id.match(/\d+/);
             }
-        } else if (this.id === "subtract") {
-            operator = "subtract";
-            if (displayValue[0] === null) {
-                displayValue[0] = screen.textContent * 1;
-                screen.textContent = "";
-            }
-        } else if (this.id === "multiply") {
-            operator = "multiply";
-            if (displayValue[0] === null) {
-                displayValue[0] = screen.textContent * 1;
-                screen.textContent = "";
-            }
-        } else if (this.id === "divide") {
-            operator = "divide";
-            if (displayValue[0] === null) {
-                displayValue[0] = screen.textContent * 1;
-                screen.textContent = "";
-            }
-        } else if (this.id === "equals") {
-             if (!(displayValue[0] === null)) {
-                displayValue[1] = screen.textContent * 1;
-                console.log(displayValue);
-                screen.textContent = operate(operator, displayValue[0], displayValue[1]);
-                displayValue[0] = null; 
-             }
+        } else if ((a === null) && (b === null) && (this.id === "add" || "subtract" || "multiply" || "divide")) {
+            operator = this.id;
+            a = screen.textContent * 1;
+        } else if ((a === null) && !(b === null) && (this.id === "add" || "subtract" || "multiply" || "divide")) {
+            a = screen.textContent * 1;
+            c = b;
+            b = a;
+            a = c;
+            a = operate(operator, a, b);
+            screen.textContent = a;
+            console.log(a,b,c);
+            a = screen.textContent * 1;
+            b = null;
+            operator = this.id;
         }
+        console.log(a,b,c);
     });
 });
